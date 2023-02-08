f8ed3a978bced28b485850b3901f7c5066328cf5
KK <krishna.kiran@thoughtworks.com>
2023-02-08 18:31:49 +0530

    Dynamic import of federated module

--- mf-host/src/components/ConsultationMF.tsx
+++ mf-host/src/components/ConsultationMF.tsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useRef } from 'react';
-import mount, { unmount } from '@bahmni-mf/consultation/ConsultationApp';
+import { importRemote } from '@module-federation/utilities';
 
 const ConsultationMF = () => {
     const ref = useRef(null);
 
     useEffect(() => {
         if (ref.current) {
-            mount(ref.current);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            importRemote<any>({
+                url: 'http://localhost:9001',
+                scope: 'bahmni_mf_consultation',
+                module: 'ConsultationApp',
+            }).then(({ default: mount, unmount }) => {
+                interface Window {
+                    mountConsultation: typeof mount;
+                    unmountConsultation: typeof unmount;
+                }
+
+                (window as unknown as Window).mountConsultation = mount;
+                (window as unknown as Window).unmountConsultation = unmount;
+                mount(ref.current);
+            });
         }
     }, []);
 
     return <div ref={ref} />;
 };
-
-interface Window {
-    mountConsultation: typeof mount;
-    unmountConsultation: typeof unmount;
-}
-
-(window as unknown as Window).mountConsultation = mount;
-(window as unknown as Window).unmountConsultation = unmount;
 
 export default ConsultationMF;
